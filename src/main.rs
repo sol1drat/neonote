a2c5df973cefaf72b1ff1d33da3f4b682703b313
sol1drat <nutitota@tutanota.com>
2026-06-26 12:34:32 +0400

    Simple hello world

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,34 @@
+use std::io;
+
+use crossterm::event::{self, Event, KeyCode};
+use ratatui::{
+    widgets::{Block, Borders, Paragraph},
+    DefaultTerminal,
+};
+
 pub enum AppState {
     Menu,
     Note,
 }
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+
+    loop {
+        terminal.draw(|frame| {
+            let hello = Paragraph::new("Hello, World!")
+                .block(Block::default().title("Hello").borders(Borders::ALL));
+
+            frame.render_widget(hello, frame.area());
+        })?;
+
+        if let Event::Key(key) = event::read()? {
+            if key.code == KeyCode::Char('q') {
+                break;
+            }
+        }
+    }
+
+    ratatui::restore();
+    Ok(())
 }
