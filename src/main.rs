258af43c7bf196e766a7e65c088a5f9dbad03772
sol1drat <nutitota@tutanota.com>
2026-06-26 13:49:09 +0400

    Implement start menu/splash screen

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,68 @@ use std::io;
 
 use crossterm::event::{self, Event, KeyCode};
 use ratatui::{
-    widgets::{Block, Borders, Paragraph},
-    DefaultTerminal,
+    layout::{
+        Alignment,
+        Constraint,
+        Direction,
+        Layout,
+    },
+    text::Line,
+    style::Stylize,
+    widgets::Paragraph,
 };
-
-pub enum AppState {
-    Menu,
-    Note,
-}
 
 fn main() -> io::Result<()> {
     let mut terminal = ratatui::init();
 
     loop {
         terminal.draw(|frame| {
-            let hello = Paragraph::new("Hello, World!")
-                .block(Block::default().title("Hello").borders(Borders::ALL));
+            let area = Layout::default()
+                .direction(Direction::Vertical)
+                .constraints([
+                    Constraint::Fill(1),
+                    Constraint::Length(9),
+                    Constraint::Fill(1),
+                ])
+                .split(frame.area())[1];
 
-            frame.render_widget(hello, frame.area());
+            let inner = Layout::default()
+                .direction(Direction::Vertical)
+                .constraints([
+                    Constraint::Length(1),
+                    Constraint::Length(1),
+                    Constraint::Length(3),
+                    Constraint::Length(1),
+                    Constraint::Length(1),
+                    Constraint::Length(1),
+                ])
+                .split(area);
+
+            let title = Paragraph::new("NeoNote".bold().blue())
+                .alignment(Alignment::Center);
+
+            let description = Paragraph::new(
+                "NNote is a keyboard-first note taking app in your terminal\n\
+                Local markdown notes, simple, quick and lightweight\n\
+                Start by opening a vault"
+            )
+            .alignment(Alignment::Center);
+
+            let vault_option = Paragraph::new(Line::from(vec![
+                    "v".bold(),
+                    " to open a vault".into(),
+            ])).alignment(Alignment::Center);
+
+            let quit_option = Paragraph::new(Line::from(vec![
+                    "q".bold(),
+                    " to quit".into(),
+            ])).alignment(Alignment::Center);
+
+
+            frame.render_widget(title, inner[0]);
+            frame.render_widget(description, inner[2]);
+            frame.render_widget(vault_option, inner[4]);
+            frame.render_widget(quit_option, inner[5]);
         })?;
 
         if let Event::Key(key) = event::read()? {
